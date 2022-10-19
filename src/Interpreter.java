c32eecb09bb66786a9d2609e4a293f53a3234389
shaima-h <shaima.hussaini@gmail.com>
2022-10-19 17:06:43 -0400

    assignment 8

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -32,27 +32,36 @@ public class Interpreter {
 	 * @param root Node that will be resolved.
 	 * @return Float representing resolution to mathematical operation.
 	 */
-	public float resolve(Node root) {
+	public static float resolve(Node root, HashMap<String, InterpreterDataType> variables) {
 			
 		//empty
 	    if (root == null)
 	        return 0;
 	 
 	    //leaf node
-	    if(root instanceof IntegerNode || root instanceof FloatNode) {
+	    if(root instanceof IntegerNode || root instanceof FloatNode || root instanceof VariableReferenceNode) {
 	    	if(root instanceof IntegerNode)
 	    		return (float)(((IntegerNode) root).number);
-	    	else
+	    	else if(root instanceof FloatNode)
 	    		return (float)(((FloatNode) root).number);
+	    	else {
+	    		if(variables.get(((VariableReferenceNode) root).name) instanceof FloatDataType) {
+		    		return (float) ((FloatDataType) (variables.get(((VariableReferenceNode) root).name))).value;
+	    		}
+	    		else {
+		    		return (float) ((IntDataType) (variables.get(((VariableReferenceNode) root).name))).value;
+	    		}
+	    		
+	    	}
 	    	
-	    }	        
+	    }
 	 
 	    if(root instanceof MathOpNode) {
 	    	//evaluate left subtree
-		    float leftEval = resolve(((MathOpNode) root).getLeft());
+		    float leftEval = resolve(((MathOpNode) root).getLeft(), variables);
 		 
 		    //evaluate right subtree
-		    float rightEval = resolve(((MathOpNode) root).getRight());
+		    float rightEval = resolve(((MathOpNode) root).getRight(), variables);
 		 
 		    //check operator
 		    if (((MathOpNode) root).getOperator() == MathOpNode.Operator.PLUS)
@@ -131,9 +140,10 @@ public class Interpreter {
 				
 		//looping through all statements
 		for(int i = 0; i < statements.size(); i++) {
-			boolean variadic = false; //if function is variadic
 			
 			if(statements.get(i) instanceof FunctionCallNode) {
+				boolean variadic = false; //if function is variadic
+				
 				FunctionCallNode functionCall = (FunctionCallNode) statements.get(i);
 				
 //				System.out.println("name: " + functionCall.name);
@@ -167,7 +177,7 @@ public class Interpreter {
 						if(variables.get(((VariableReferenceNode)(functionCall).parameters.get(j).parameter).name) instanceof IntDataType) {
 							values.add(new IntDataType(((IntDataType) variables.get(((VariableReferenceNode)(functionCall).parameters.get(j).parameter).name)).value));
 						}
-						else { //FloatDataType
+						else if(variables.get(((VariableReferenceNode)(functionCall).parameters.get(j).parameter).name) instanceof FloatDataType) {
 							values.add(new FloatDataType(((FloatDataType) variables.get(((VariableReferenceNode)(functionCall).parameters.get(j).parameter).name)).value));
 						}
 												
@@ -254,11 +264,162 @@ public class Interpreter {
 				
 				
 			}
+			else if(statements.get(i) instanceof AssignmentNode) {
+				
+				System.out.println("Before assignment:" + variables);
+				
+				AssignmentNode assignment = (AssignmentNode) statements.get(i);
+				
+				if(variables.get(assignment.target.name) instanceof FloatDataType) {
+					float result = resolve(assignment.expression, variables);
+					variables.replace(assignment.target.name, new FloatDataType(result));
+				}
+				else if(variables.get(assignment.target.name) instanceof IntDataType) {
+					int result = (int) resolve(assignment.expression, variables);
+					variables.replace(assignment.target.name, new IntDataType(result));
+				}
+				
+				System.out.println("After assignment:" + variables);
+				
+			}
+			else if(statements.get(i) instanceof WhileNode) {
+				WhileNode whileNode = (WhileNode) statements.get(i);
+				boolean condition = evaluateBooleanExpression(whileNode.expression, variables);
+				
+				while(condition) {
+					interpretBlock(whileNode.statements, variables);
+					condition = evaluateBooleanExpression(whileNode.expression, variables);
+				}
+			}
+			else if(statements.get(i) instanceof RepeatNode) {
+				RepeatNode repeatNode = (RepeatNode) statements.get(i);
+				boolean condition;
+
+				do {
+					interpretBlock(repeatNode.statements, variables);
+					condition = evaluateBooleanExpression(repeatNode.expression, variables);
+				} while(condition);
+			}
+			else if(statements.get(i) instanceof IfNode) {
+				IfNode ifNode = (IfNode) statements.get(i);
+				
+				if(evaluateBooleanExpression(ifNode.expression, variables)) {
+					interpretBlock(ifNode.statements, variables);
+				}
+				else {
+					ifNode = ifNode.ifNode;
+					while(ifNode != null) {
+						if(ifNode instanceof ElseNode) {
+							interpretBlock(((ElseNode) ifNode).statements, variables);
+							break;
+						}
+						
+						if(evaluateBooleanExpression(ifNode.expression, variables)) {
+							interpretBlock(ifNode.statements, variables);
+							break;
+						}
+						
+						ifNode = ifNode.ifNode;
+					}
+				}
+			}
+			else if(statements.get(i) instanceof ForNode) {
+				ForNode forNode = (ForNode) statements.get(i);
+				int start = 0;
+				int end = 0;
+				
+				if(forNode.start instanceof IntegerNode) {
+					start = ((IntegerNode) forNode.start).number;
+				}
+				else {
+					throw new Exception("For node start value must be an integer.");
+				}
+				
+				if(forNode.end instanceof IntegerNode) {
+					end = ((IntegerNode) forNode.end).number;
+				}
+				else {
+					throw new Exception("For node end value must be an integer.");
+				}
+				
+				if(start < end) {
+					for(int f = start; f < end; f++) {
+						variables.replace(forNode.var.name, new IntDataType(f));
+						interpretBlock(forNode.statements, variables);
+					}
+				}
+				else {
+					for(int f = start; f < end; f++) {
+						variables.replace(forNode.var.name, new IntDataType(f));
+						interpretBlock(forNode.statements, variables);
+					}
+				}				
+			}
+			
 			else {
 				continue;
 			}
 		}
 		
+	}
+	
+	
+	public static boolean evaluateBooleanExpression(BooleanExpressionNode expression, HashMap<String, InterpreterDataType> variables) {
+		
+		//FloatNode, IntegerNode, MathOpNode, VariableReferenceNode
+		float left = 0;
+		
+		if(expression.left instanceof IntegerNode) {
+			left = (int) resolve(expression.left, variables);
+		}
+		else if(expression.left instanceof FloatNode || expression.left instanceof MathOpNode) {
+			left = (float) resolve(expression.left, variables);
+		}
+		else if(expression.left instanceof VariableReferenceNode) {
+			if(variables.get(((VariableReferenceNode) expression.left).name) instanceof FloatDataType) {
+				left = (float) resolve(expression.left, variables);
+			}
+			else if(variables.get(((VariableReferenceNode) expression.left).name) instanceof IntDataType) {
+				left = (int) resolve(expression.left, variables);
+			}
+			
+		}
+		
+		float right = 0;
+		
+		if(expression.right instanceof IntegerNode) {
+			right = (int) resolve(expression.right, variables);
+		}
+		else if(expression.right instanceof FloatNode || expression.right instanceof MathOpNode) {
+			right = (float) resolve(expression.right, variables);
+		}
+		else if(expression.right instanceof VariableReferenceNode) {
+			if(variables.get(((VariableReferenceNode) expression.right).name) instanceof FloatDataType) {
+				right = (float) resolve(expression.right, variables);
+			}
+			else if(variables.get(((VariableReferenceNode) expression.right).name) instanceof IntDataType) {
+				right = (int) resolve(expression.right, variables);
+			}
+		}
+				
+		switch(expression.condition) {
+		case GREATER_THAN:
+			return (left > right);
+		case GREATER_EQUAL:
+			return (left >= right);
+		case LESS_THAN:
+			return (left < right);
+		case LESS_EQUAL:
+			return (left <= right);
+		case NOT_EQUALS:
+			return (left != right);
+		case EQUALS:
+			return (left == right);
+		default:
+			return false;
+			
+		}		
+			
 	}
 	
 	
